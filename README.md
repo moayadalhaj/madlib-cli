fcf9c6575b190faa0c3f20ae3953cf39b64e804c
moayadalhaj <moayad.alhaj21@gmail.com>
2021-10-12 23:07:03 +0300

    create a madlib cli game that use the inputs from user and add it to the file content

---
+++ README.md
@@ -0,0 +1,11 @@
+# Madlib Game
+
+## Create a madlib game, a functionality programe that allows you to input a specific input depend on the word appear in the question a fun game based on your inputs
+
+Problem Domain: You should create a functionality that takes multiple inputs, and merge them to a txt file, then print the output
+
+Start time : 7:00 pm
+
+Finish time: 10:00 pm
+
+I learned alot of new functionalities while solving this lab like file IO and about regex also using unpaking symbols.
---
+++ madlib_cli/madlib.py
@@ -0,0 +1,32 @@
+import re
+
+print('Welcom to madlib game, a functionality programe that takes multiple inputs')
+
+def read_template(file_name):
+    try:
+        with open(file_name) as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return 'File Not Found'
+
+def parse_template(content):
+    try:
+        content_in_curly_braces = re.findall('{(.+?)}',content)
+        return content_in_curly_braces
+    except:
+        return 'an error occured while tracing the curlies'
+
+def inputs(content_in_curly_braces):
+    user_inputs = []
+    for i in range(len(content_in_curly_braces)):
+        user_input=input(f'Please Enter {content_in_curly_braces[i]}: ')
+        user_inputs.append(user_input)
+    return user_inputs
+
+def merge(file_content,user_inputs):
+    file_content=re.sub(r"\{.*?\}","{}",file_content)
+    return file_content.format(*user_inputs)
+
+
+#print(merge(read_template('assets/dark_and_stormy_night_template.txt'),inputs(parse_template(read_template('assets/dark_and_stormy_night_template.txt')))))
